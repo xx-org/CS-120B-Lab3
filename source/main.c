e8e7d2cc117109e578742aa500bf28f925ba4ee6
Jia Xie <jxie031@wch136-16.cs.ucr.edu>
2019-06-26 11:51:01 -0700

    init

---
+++ source/main.c
@@ -0,0 +1,25 @@
+/*	Author: jxie031
+ *  Partner(s) Name: Xichao Wang
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+		
+
+    }
+    return 1;
+}
